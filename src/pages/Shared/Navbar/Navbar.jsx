0ca632f9ad5e1c1bbfcc7375246a5c99997091d4
jaheda8256 <jahedasultana47@gmail.com>
2024-04-10 10:46:42 +0600

    added register pages

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,7 +1,16 @@
 import { Link, NavLink } from "react-router-dom";
 import userDefaultPic from '../../../assets/user.png';
+import { useContext } from "react";
+import { AuthContext } from "../../../provider/AuthProvider";
 
 const Navbar = () => {
+
+  const {user, logOut}  = useContext(AuthContext);
+  const handleSignOut = () =>{
+    logOut()
+    .then()
+    .catch()
+  }
   const navLinks = (
     <>
       <li>
@@ -59,9 +68,14 @@ const Navbar = () => {
           <img alt="Tailwind CSS Navbar component" src={userDefaultPic}/>
         </div>
       </div>
-        <Link to='/login'>
+      {
+        user ?
+        <button onClick={handleSignOut} className="btn">Sign Out</button>
+        :  <Link to='/login'>
         <button className="btn">Login</button>
         </Link>
+      }
+      
         
     </div>
     </div>
